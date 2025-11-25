424bab6d464262aaff3f723e8e3ba9f7fb08c7cb
Indra Saepudin <indrasaepudin212@gmail.com>
2025-11-25 22:30:55 +0700

    test deploy prisma

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,31 @@
 // src/config/db.js
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+// export const prisma = new PrismaClient();
+
+// src/config/db.js
+import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig, Pool } from "@neondatabase/serverless";
+import ws from "ws";
+
+// konfigurasi WebSocket untuk Neon
+neonConfig.webSocketConstructor = ws;
+
+// buat pool Neon
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// adapter Prisma untuk Neon
+const adapter = new PrismaNeon(pool);
+
+// inisiasi Prisma Client dengan adapter Neon
+// supaya tidak bikin banyak instance di serverless, bisa pakai pattern global
+const prisma = global.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
+
+export { prisma };
