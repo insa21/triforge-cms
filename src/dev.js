cc4f49a6381967fbf927a0a88dcf0533b8084995
Indra Saepudin <indrasaepudin212@gmail.com>
2025-11-27 03:04:38 +0700

    ssss

--- src/dev.js
+++ src/dev.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+// app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 // health check
 app.get("/", (req, res) => {
--- src/serv.js
+++ src/serv.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+// app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.get("/", (req, res) => {
   res.json({
